91fad773dbc4e46085dab9d4fb5203ddef2a7ba2
frank1966 <serviciopaco@hotmail.com>
2026-08-18 18:09:48 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Mientras dejes estos valores como están, el formulario funciona en "modo demo":
 // valida los campos y muestra el mensaje de éxito, pero no guarda nada.
 
-window.SUPABASE_URL = "";
-window.SUPABASE_ANON_KEY = "";
+window.SUPABASE_URL = "https://woavktvpmitndxktvlkw.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvYXZrdHZwbWl0bmR4a3R2bGt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2NTEyMzMsImV4cCI6MjEwMjIyNzIzM30.VYXiTh2MSrMpHgcedJnlhm44pP55b2-JN9bVKXaxlfc";
